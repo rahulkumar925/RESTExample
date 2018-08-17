a0c244669163327ceab35d086f70924e6747e6f4
Rahul kumar <rockingrahul925@gmail.com>
2018-08-17 17:24:58 +0530

    Add files via upload

---
+++ rest/src/main/java/com/cg/rest/RestApplication.java
@@ -0,0 +1,18 @@
+package com.cg.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class RestApplication
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	SpringApplication.run(RestApplication.class, args);
+    }
+}
